f41140191436d9797805e04e8454ac867e1a6426
mahalakshmi34 <tasmahalakshmi@gmail.com>
2022-03-24 13:48:02 +0530

    delivery package screen UI

---
+++ MemeMove/DeliveryPackageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DeliveryPackageViewController.swift
+//  MemeMove
+//
+//  Created by Vijay Raj on 24/03/22.
+//
+
+import UIKit
+
+class DeliveryPackageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
