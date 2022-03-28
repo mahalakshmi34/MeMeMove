73fa670b801e29a658c92f8e9ca6571313dc7872
mahalakshmi34 <tasmahalakshmi@gmail.com>
2022-03-28 13:31:16 +0530

    addAddress UI

---
+++ MemeMove/Select Vehicle Type ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Select Vehicle Type ViewController.swift
+//  MemeMove
+//
+//  Created by Vijay Raj on 28/03/22.
+//
+
+import UIKit
+
+class Select_Vehicle_Type_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
