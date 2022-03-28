6c699a2d46d0957b3b76c1244e5fb53c89b20d75
mahalakshmi34 <tasmahalakshmi@gmail.com>
2022-03-28 18:03:16 +0530

    vehicletype UI

---
+++ MemeMove/VehicleTypeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  VehicleTypeCollectionViewCell.swift
+//  MemeMove
+//
+//  Created by Vijay Raj on 28/03/22.
+//
+
+import UIKit
+
+class VehicleTypeCollectionViewCell: UICollectionViewCell {
+    
+}
