883181890f1d5c17610747f944e74ea5e40badb5
mahalakshmi34 <tasmahalakshmi@gmail.com>
2022-05-12 16:29:03 +0530

    checkbox functionality

---
+++ MemeMove/Controller/SelectPackageContent/PackageContentTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PackageContentTableViewCell.swift
+//  MemeMove
+//
+//  Created by Vijay Raj on 12/05/22.
+//
+
+import UIKit
+
+class PackageContentTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var packageLabel: UILabel!
+    
+    @IBOutlet weak var checkBoxBtn: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //self.checkBoxBtn.addTarget(self, action: #selector(subscribeButtonTapped(_:)), for: .touchUpInside)
+    
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
