6613c9cc49c9cb9fc7a35c24024523030def6446
mahalakshmi34 <tasmahalakshmi@gmail.com>
2022-07-12 21:32:01 +0530

    adding feature

---
+++ MemeMove/TotalRidesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TotalRidesTableViewCell.swift
+//  MemeMove
+//
+//  Created by Vijay Raj on 09/07/22.
+//
+
+import UIKit
+
+class TotalRidesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MemeMove/TotalRidesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TotalRidesViewController.swift
+//  MemeMove
+//
+//  Created by Vijay Raj on 09/07/22.
+//
+
+import UIKit
+
+class TotalRidesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
